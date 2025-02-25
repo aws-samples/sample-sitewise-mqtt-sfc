b2575a093f434b4ae8f6f58e5f6f9e4c30e320dd
Hubert Asamer <asamerh4@users.noreply.github.com>
2025-02-25 22:47:09 +0100

    Update install.sh

--- sfc/install.sh
+++ sfc/install.sh
@@ -2,7 +2,7 @@ export VERSION="v1.8.3"
 export SFC_DEPLOYMENT_DIR=$(pwd)
 
 wget https://github.com/aws-samples/shopfloor-connectivity/releases/download/$VERSION/\
-{mqtt-target,aws-sitewiseedge-target,debug-target,modbus-tcp,sfc-main}.tar.gz
+{mqtt-target,debug-target,modbus-tcp,sfc-main}.tar.gz
 
 for file in *.tar.gz; do
   tar -xf "$file"
